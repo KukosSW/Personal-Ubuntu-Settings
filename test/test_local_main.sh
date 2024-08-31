d30ee0d529589327a931fff1f80c475cfa7fc6ae
Michał Kukowski <kukossw@gmail.com>
2024-08-31 15:30:01 +0200

    [FEATURE] Simple Message

--- test/test_local_main.sh
+++ test/test_local_main.sh
@@ -14,9 +14,25 @@ function Test::Utils::pass()
     printf "%b[TEST] PASSED: ${1}%b\n" "\033[0;32m" "\033[0m"
 }
 
+function Test::Utils::shellcheck_check()
+{
+    local script="${1}"
+    if shellcheck --shell=bash --enable=all --external-sources "${script}"; then
+        Test::Utils::pass "Shellcheck: ${script}"
+    else
+        Test::Utils::fail "Shellcheck: ${script}"
+    fi
+}
+
+function Test::TestCase::shellcheck()
+{
+    Test::Utils::shellcheck_check "${PROJECT_TOP_DIR}/src/message.sh"
+}
+
 function Test::TestSuite::run()
 {
-    Test::Utils::pass "Test::Utils::pass"
+
+    Test::TestCase::shellcheck
 }
 
 Test::TestSuite::run
