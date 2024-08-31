524149895d76961646d0a150f7e00b48ace34f9c
Michał Kukowski <kukossw@gmail.com>
2024-08-31 15:29:39 +0200

    [TEST] Add simple test prints

--- test/test_local_main.sh
+++ test/test_local_main.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-echo "Test!!!"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_TOP_DIR="${SCRIPT_DIR}/.."
 
+function Test::Utils::fail()
+{
+    printf "%b[TEST] FAILED: ${1}%b\n" "\033[0;31m" "\033[0m"
+    exit 1
+}
+
+function Test::Utils::pass()
+{
+    printf "%b[TEST] PASSED: ${1}%b\n" "\033[0;32m" "\033[0m"
+}
+
+function Test::TestSuite::run()
+{
+    Test::Utils::pass "Test::Utils::pass"
+}
+
+Test::TestSuite::run
