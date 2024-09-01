a1d709ee572aa7a377a77708e44e48759e8de31e
Michal Kukowski <kukossw@gmail.com>
2024-09-01 14:40:25 +0200

    [FEATURE] E2E Local and CI testing

--- test/test_docker_main.sh
+++ test/test_docker_main.sh
@@ -4,6 +4,8 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_TOP_DIR="${SCRIPT_DIR}/.."
 
+####################################### TEST UNIT #######################################
+
 # Test Ubuntu Latest
 DOCKER_BUILDKIT=1 docker build --file ${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_latest --tag docker_ubuntu_latest ${PROJECT_TOP_DIR}
 docker run --rm docker_ubuntu_latest || exit 1
@@ -15,3 +17,17 @@ docker run --rm docker_ubuntu_2404 || exit 1
 # Test Ubuntu 24.10
 DOCKER_BUILDKIT=1 docker build --file ${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_2410 --tag docker_ubuntu_2410 ${PROJECT_TOP_DIR}
 docker run --rm docker_ubuntu_2410 || exit 1
+
+####################################### TEST END-TO-END #######################################
+
+# Test Ubuntu Latest
+DOCKER_BUILDKIT=1 docker build --file ${PROJECT_TOP_DIR}/test/docker/Dockerfile_e2e_ubuntu_latest --tag docker_e2e_ubuntu_latest ${PROJECT_TOP_DIR}
+docker run --rm docker_e2e_ubuntu_latest || exit 1
+
+# Test Ubuntu 24.04
+DOCKER_BUILDKIT=1 docker build --file ${PROJECT_TOP_DIR}/test/docker/Dockerfile_e2e_ubuntu_2404 --tag docker_e2e_ubuntu_2404 ${PROJECT_TOP_DIR}
+docker run --rm docker_e2e_ubuntu_2404 || exit 1
+
+# Test Ubuntu 24.10
+DOCKER_BUILDKIT=1 docker build --file ${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_2410 --tag docker_e2e_ubuntu_2410 ${PROJECT_TOP_DIR}
+docker run --rm docker_e2e_ubuntu_2410 || exit 1
