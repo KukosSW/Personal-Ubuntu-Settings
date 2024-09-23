1ce36c15765b1741b49be52cd6067ac8dce72534
Michal Kukowski <kukossw@gmail.com>
2024-09-24 00:07:08 +0200

    [FIX] Fix path in run.sh to main.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 # This is the MAIN file for the project.
 # As a user you should only run this file.
-$(pwd)/src/main.sh
+
+# Get the directory of this script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_TOP_DIR="${SCRIPT_DIR}/"
+
+${PROJECT_TOP_DIR}/src/main.sh
