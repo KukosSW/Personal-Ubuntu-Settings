879ecff178b97ff37b21cacabd7a193279782a6a
Michal Kukowski <kukossw@gmail.com>
2024-11-18 14:42:10 +0100

    [FEATURE] plocate (locate) installation

--- src/install_cli_utils.sh
+++ src/install_cli_utils.sh
@@ -45,6 +45,7 @@ PersonalSettings::Installer::install_cli_utils()
     PersonalSettings::PackageManager::Apt::install "sed" || return 1
     PersonalSettings::PackageManager::Apt::install "grep" || return 1
     PersonalSettings::PackageManager::Apt::install "apt-rdepends" || return 1
+    PersonalSettings::PackageManager::Apt::install "plocate" || return 1
 
 
     # MONITORING UTILITIES
