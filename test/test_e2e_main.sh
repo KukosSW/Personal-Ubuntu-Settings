88e13386194affcec0f348f97932d38745c092aa
Michal Kukowski <kukossw@gmail.com>
2024-11-23 23:34:50 +0100

    [FIX] Added fonst tests to the E2E tests

--- test/test_e2e_main.sh
+++ test/test_e2e_main.sh
@@ -32,6 +32,7 @@ function Test::TestSuite::E2E::run()
     Test::TestCase::install_c_cpp_devtools
     Test::TestCase::install_cli_utils
     Test::TestCase::install_docker
+    Test::TestCase::install_fonts
     Test::TestCase::install_git
     Test::TestCase::install_latex
     Test::TestCase::install_vagrant
